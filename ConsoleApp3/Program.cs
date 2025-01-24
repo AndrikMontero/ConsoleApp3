76625be57fda4c06ec2a8d29f8626cb793d9b862
Andrik Montero <andrik.gm42@gmail.com>
2025-01-23 22:27:29 -0600

    version 1.0

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿# include <iostream>
+using namespace std;
+int main()
+{
+    cout >> "hola"; 
+
+
+
+
+}
